320d336b70ea4545cba808f1d3248fa47bd016c2
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-12 19:10:06 +0900

    Update example.asm

--- example.asm
+++ example.asm
@@ -4,11 +4,13 @@ section .text
 _start:
 	mov r8, 1
 	mov r9, 2
+	add r8, r9
+	mov r9, 2
 	mov r10, 3
 	mov r11, 4
-	mov r12, 5
-	add r11, r12
-	add r10, r11
+	sub r10, r11
+	add r9, r10
+	mov r10, 1
 	add r9, r10
 	add r8, r9
 
