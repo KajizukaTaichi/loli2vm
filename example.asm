4867b6dcb761fabd27139ff7afefc8d683372b4b
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-12 19:35:40 +0900

    Update example.asm

--- example.asm
+++ example.asm
@@ -5,13 +5,13 @@ _start:
 	mov r8, 1
 	mov r9, 2
 	add r8, r9
-	mov r9, 2
+	mov r9, 10
 	mov r10, 3
 	mov r11, 4
 	sub r10, r11
 	add r9, r10
-	mov r10, 1
-	add r9, r10
+	mov r10, 5
+	sub r9, r10
 	add r8, r9
 
 	mov rax, 0x2000001
