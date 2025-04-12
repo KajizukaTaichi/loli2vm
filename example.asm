7941cf1ae34d6ba7b21ae4e9fe884ddf43b193c6
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-12 19:07:54 +0900

    Update example.asm

--- example.asm
+++ example.asm
@@ -2,11 +2,13 @@ section .text
 	global _start
 
 _start:
-	mov r8, 2
-	mov r9, 3
-	add r8, r9
-	mov r9, 1
+	mov r8, 1
+	mov r9, 2
+	mov r10, 3
+	add r9, r10
 	sub r8, r9
+	mov r9, 10
+	add r8, r9
 
 	mov rax, 0x2000001
 	mov rdi, r8
